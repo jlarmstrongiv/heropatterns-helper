f93cf076ab36df13f9ee3da32790b1ffcd212f51
John Armstrong <jlarmst@g.clemson.edu>
2019-07-07 12:38:34 -0400

    feat: initial commit to split package

---
+++ example/example.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const fs = require('fs-extra');
+const fg = require('fast-glob');
+
+const heroPatternHelper = require('../dist/index');
+
+const FileType = heroPatternHelper.FileType;
+
+const samplePattern = path.join(__dirname, './sample/**.txt');
+const outputFolder = path.join(__dirname, './output');
+
+const exportSample = async samplePath => {
+  const heroPatternFile = await fs.readFile(samplePath);
+  const heroPatternText = await heroPatternFile.toString().trim();
+
+  const heroPatternMeta = await heroPatternHelper.getMetadata(heroPatternText);
+  const heroPatternSvg = heroPatternMeta.svg;
+
+  const outputName = path.basename(samplePath).replace(/\.[^/.]+$/, '');
+
+  await fs.ensureDir(`${outputFolder}/${outputName}`);
+  // jpeg
+  const jpeg = await heroPatternHelper.toJpeg(heroPatternSvg);
+  await fs.writeFile(`${outputFolder}/${outputName}/${outputName}.jpeg`, jpeg);
+  // png
+  const png = await heroPatternHelper.toPng(heroPatternSvg);
+  await fs.writeFile(`${outputFolder}/${outputName}/${outputName}.png`, png);
+  // svg
+  const svg = await heroPatternHelper.toSvg(heroPatternSvg);
+  await fs.writeFile(`${outputFolder}/${outputName}/${outputName}.svg`, svg);
+
+  const files = {
+    [FileType.JPEG]: jpeg,
+    [FileType.PNG]: png,
+    [FileType.SVG]: svg,
+  };
+
+  const zip = await heroPatternHelper.toZip(
+    heroPatternText,
+    files,
+    outputName,
+    'MIT would be nice',
+  );
+  await fs.writeFile(`${outputFolder}/${outputName}/${outputName}.zip`, zip);
+};
+
+const start = async () => {
+  await fs.remove(outputFolder);
+  const samplePaths = await fg(samplePattern);
+  await Promise.all(samplePaths.map(exportSample));
+};
+
+try {
+  start();
+} catch (error) {
+  console.log(error);
+}
---
+++ src/index.js
@@ -0,0 +1,92 @@
+import AdmZip from 'adm-zip';
+
+// https://github.com/sindresorhus/import-lazy#usage-with-bundlers
+import importLazy from 'import-lazy';
+const sharp = importLazy(() => require('sharp'))();
+
+export const FileType = {
+  JPEG: 'JPEG',
+  PNG: 'PNG',
+  SVG: 'SVG',
+  ZIP: 'ZIP',
+};
+
+export const getMetadata = heroPatternText => {
+  // regex must be reset
+  // https://stackoverflow.com/questions/4724701/regexp-exec-returns-null-sporadically
+  const widthRegex = /width='(\d+)'/g;
+  const heightRegex = /height='(\d+)'/g;
+  const svgRegex = /data:image\/svg\+xml,(.*)"\);/g;
+  const backgroundColorRegex = /background-color: #(.{1,6});/g;
+
+  heroPatternText = heroPatternText.trim();
+
+  // https://regexr.com/
+  const width = widthRegex.exec(heroPatternText)[1];
+  const height = heightRegex.exec(heroPatternText)[1];
+  let originalSvg = svgRegex.exec(heroPatternText);
+  originalSvg = decodeURIComponent(originalSvg[1]);
+
+  // add background color
+  // viewport fill not supported
+  // https://stackoverflow.com/a/11293812
+  // rectangle must be drawn first
+  // https://stackoverflow.com/a/25302276
+  const backgroundColor = backgroundColorRegex.exec(heroPatternText)[1];
+  const bracketIndex = originalSvg.indexOf('>') + 1;
+  const editedSvg = `${originalSvg.slice(
+    0,
+    bracketIndex,
+  )}<rectwidth='100%'height='100%'fill='#${backgroundColor}'></rect>${originalSvg.slice(
+    bracketIndex,
+    originalSvg.length,
+  )}`;
+  return {
+    width,
+    height,
+    backgroundColor,
+    svg: editedSvg,
+  };
+};
+
+export const toJpeg = async heroPatternSvg => {
+  if (!heroPatternSvg) throw new Error('No pattern provided');
+
+  // svg input must be buffer
+  // https://github.com/lovell/sharp/issues/979#issuecomment-334931655
+  const image = await sharp(Buffer.from(heroPatternSvg))
+    .toFormat('jpeg')
+    .toBuffer();
+
+  return image;
+};
+
+export const toPng = async heroPatternSvg => {
+  if (!heroPatternSvg) throw new Error('No pattern provided');
+
+  const image = await sharp(Buffer.from(heroPatternSvg))
+    .toFormat('png')
+    .toBuffer();
+
+  return image;
+};
+
+export const toSvg = async heroPatternSvg => {
+  if (!heroPatternSvg) throw new Error('No pattern provided');
+  return Buffer.from(heroPatternSvg);
+};
+
+export const toZip = async (heroPatternSvg, files = {}, fileName, licenseText = '') => {
+  const jpeg = files[FileType.JPEG] || (await toJpeg(heroPatternSvg));
+  const png = files[FileType.PNG] || (await toPng(heroPatternSvg));
+  const svg = files[FileType.SVG] || (await toSvg(heroPatternSvg));
+
+  const zip = new AdmZip();
+
+  zip.addFile(fileName ? `${fileName}.jpeg` : 'heropattern.jpeg', jpeg);
+  zip.addFile(fileName ? `${fileName}.png` : 'heropattern.png', png);
+  zip.addFile(fileName ? `${fileName}.svg` : 'heropattern.svg', svg);
+  zip.addFile('LICENSE.txt', Buffer.from(licenseText));
+
+  return zip.toBuffer();
+};
